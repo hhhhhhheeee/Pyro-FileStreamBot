a09e6814bcb529b9eb41f0be25f8afb778bf20ae
hhhhhhheeee <79951826+hhhhhhheeee@users.noreply.github.com>
2021-08-07 21:25:55 +0530

    Update start.py

--- WebStreamer/bot/plugins/start.py
+++ WebStreamer/bot/plugins/start.py
@@ -53,7 +53,7 @@ async def start(b, m):
                     disable_web_page_preview=True)
                 return
         await m.reply_text(
-            text='├ 🧬 Bot Owner : @ShivamTheSkywalker\n\├ 💾 Functionality : File to link\n\├ 🛡Support group : 😬\n\├ 🛠Channel : @XStream_Flix\n\├ 🔼 Link Max Speed : 4MB/s\n\├ 🧲Forward File to get link\n\╰──「 🚸@XStream_Flix🚸 」',
+            text='├ 🧬 Bot Owner : @ShivamTheSkywalker*\n\├ 💾 Functionality : File to link*\n\├ 🛡Support group : 😬*\n\├ 🛠Channel : @XStream_Flix*\n\├ 🔼 Link Max Speed : 4MB/s*\n\├ 🧲Forward File to get link*\n\╰──「 🚸@XStream_Flix🚸 」',
             reply_markup=InlineKeyboardMarkup(
                 [
                     [InlineKeyboardButton('Report Error', url='https://t.me/ShivamTheSkywalker')]
